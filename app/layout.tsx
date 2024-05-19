0d52d7816917fd2f6bd14d9cedd143974d9b3501
Liang Chen <liangyu.chen.dev@gmail.com>
2024-05-18 23:59:39 -0400

    refactor: Update favicon and meta data in layout.tsx
    The code changes in `layout.tsx` involve updating the favicon and meta data. This includes adding links to different favicon images, updating the manifest file for the site, and adding a shortcut icon. These changes improve the appearance and accessibility of the website.

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,32 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* favicon */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#000000"
+        />
+        {/* meta data */}
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Emoji Picker</title>
